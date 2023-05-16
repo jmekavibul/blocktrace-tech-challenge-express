bd46e1a0e976411a8abb085bf9bfde800e5f0c83
James Mekavibul <31485121+jmekavibul@users.noreply.github.com>
2023-05-16 02:56:01 -0400

    remove console.log for now

--- routes/submit.js
+++ routes/submit.js
@@ -4,22 +4,21 @@ const axios = require('axios');
 
 /* Get Info From Submit */
 router.post('/', function(req, res, next) {
-  const { address, apikey, cryptoaddresstype } = req.body;
+  const { address, apiKey, addressType } = req.body;
   const addresses = address.split("\n")
   console.log(addresses);
   const provider = 'ca';
   const url = `https://fusion.blocktrace.com/api/v1/bulk/${provider}/cluster/summary`;
   const headers = {
     'Content-Type': 'application/json',
-    'api_key': apikey 
+    'api_key': apiKey 
   };
   const data = JSON.stringify({
     addresses: addresses,
   });
 
-  axios.post(url, data, { params : {'type': cryptoaddresstype }, headers })
+  axios.post(url, data, { params : {'type': addressType }, headers })
     .then((response) => {
-      console.log(response.data); // Log the response from the third-party API
       res.send(response.data); // Send the response back to the client
     })
     .catch((error) => {
