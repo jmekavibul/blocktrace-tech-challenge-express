9a949c1d485c8b27ada0097c94daba4cd60eaa5b
James Mekavibul <31485121+jmekavibul@users.noreply.github.com>
2023-05-16 02:58:36 -0400

    Add better comments

--- routes/submit.js
+++ routes/submit.js
@@ -5,23 +5,24 @@ const axios = require('axios');
 /* Get Info From Submit */
 router.post('/', function(req, res, next) {
   const { address, apiKey, addressType } = req.body;
-  const addresses = address.split("\n")
-  const provider = 'ca';
+  const addresses = address.split("\n"); // Assume the input is similar to that of the email
+  const provider = 'ca'; // For future providers
   const url = `https://fusion.blocktrace.com/api/v1/bulk/${provider}/cluster/summary`;
   const headers = {
     'Content-Type': 'application/json',
     'api_key': apiKey 
   };
+
   const data = JSON.stringify({
     addresses: addresses,
   });
 
   axios.post(url, data, { params : {'type': addressType }, headers })
     .then((response) => {
-      res.send(response.data); // Send the response back to the client
+      res.send(response.data);
     })
     .catch((error) => {
-      console.log(error); // Log any errors
+      console.log(error);
       res.status(500).send('Internal Server Error');
     });
 });
