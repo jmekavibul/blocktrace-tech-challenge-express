2bdb027d015901b8c2e906eee60a5d64b8237a70
James Mekavibul <31485121+jmekavibul@users.noreply.github.com>
2023-05-16 23:03:41 -0400

    Add better error handling

--- routes/submit.js
+++ routes/submit.js
@@ -23,7 +23,7 @@ router.post('/', function(req, res, next) {
     })
     .catch((error) => {
       console.log(error);
-      res.status(500).send('Internal Server Error');
+      res.status(error.response.status).send(error.response.data.message);
     });
 });
 
